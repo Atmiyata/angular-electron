f99fb7242e69b86b5fc0c11ccbeb9cbe4454f1cc
jayraj <jayrajsolanki50@gmail.com>
2022-01-16 12:28:36 -0500

    Electron app setup with angular

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(path.join(__dirname, `/dist/angular-electron/index.html`));
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
